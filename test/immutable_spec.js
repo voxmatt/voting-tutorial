ec59f1f3d8edb3c338ed99332518ba424cd18d79
Matt Martin <mattm@relateiq.com>
2016-03-11 15:40:06 -1000

    adding a super basic, redundent test just to see how framework works

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -2,16 +2,23 @@ import { expect } from 'chai';
 
 describe('immutability', () => {
   describe('a number', () => {
-    function increment(currentState) {
+    function addMovie(currentState) {
       return currentState + 1;
     }
 
     it('is immutable', () => {
-      const state = 42;
-      const nextState = increment(state);
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
 
-      expect(nextState).to.equal(43);
-      expect(state).to.equal(42);
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
     });
   });
 });
